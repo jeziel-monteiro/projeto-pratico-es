1f50e4dea43ac3ed1d3ad20e759187601159904d
Jeziel Monteiro Pessoa <jeziel.monteiro@ufam.edu.br>
2026-06-26 10:16:10 -0400

    arquivos de testes e remoção de arquivos

--- TP3/test/CadastroViajante_test.dart
+++ TP3/test/CadastroViajante_test.dart
@@ -9,8 +9,9 @@ void main() {
     test('Caso 1: Conta gerada com sucesso para maior de idade (Classe 13, 15)', () {
       expect(
         () => service.completarCadastro(
-          'João', 'Silva', 'joao@email.com', 
-          '15/05/2000', // Adulto (Nascido em 2000) -> Classe 13
+          'João', 'Silva', 
+          'joao.caso1@email.com', // E-mail único para este teste
+          '15/05/2000', 
           'Forte@2026', 'Forte@2026'
         ),
         returnsNormally,
@@ -20,8 +21,9 @@ void main() {
     test('Caso 10: Cadastro rejeitado por restrição de idade (Classe 14)', () {
       expect(
         () => service.completarCadastro(
-          'João', 'Silva', 'joao@email.com', 
-          '10/10/2015', // Menor de idade (Nascido em 2015) -> Classe 14
+          'João', 'Silva', 
+          'joao.crianca@email.com', // E-mail único para este teste
+          '10/10/2015', 
           'Forte@2026', 'Forte@2026'
         ),
         throwsA(isA<Exception>().having((e) => e.toString(), 'mensagem', contains('Restrição de idade'))),
@@ -31,7 +33,8 @@ void main() {
     test('Caso Extra: Cadastro rejeitado por senhas divergentes (Classe 21)', () {
       expect(
         () => service.completarCadastro(
-          'João', 'Silva', 'joao@email.com', 
+          'João', 'Silva', 
+          'joao.errosenha@email.com', // E-mail único para este teste
           '15/05/2000', 
           'Forte@2026', 'Fraca123'
         ),
